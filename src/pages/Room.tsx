a66c39d0a7f7174a40f91194a217c0beba97083b
Mateus Castro <mateuscastro.dev@gmail.com>
2022-01-22 16:11:27 -0300

    room page structure and new question form

--- src/pages/Room.tsx
+++ src/pages/Room.tsx
@@ -1,7 +1,45 @@
 import { useParams } from 'react-router-dom'
 
-export function Room() {
-  const { roomKey } = useParams<{ roomKey: string }>()
+import logoImage from '../assets/images/logo.svg'
 
-  return <h1>{`Sala ${roomKey}`}</h1>
+import { Button } from '../components/Button'
+import { RoomCode } from '../components/RoomCode'
+
+import '../styles/room.scss'
+
+type RoomParams = {
+  roomKey: string
+}
+
+export function Room() {
+  const { roomKey } = useParams<RoomParams>()
+
+  return (
+    <div id='page-room'>
+      <header>
+        <div className='content'>
+          <img src={logoImage} alt='letmeask' />
+          <RoomCode code={roomKey} />
+        </div>
+      </header>
+
+      <main>
+        <div className='room-title'>
+          <h1>Aula de React JS</h1>
+          <span>10 perguntas</span>
+        </div>
+
+        <form>
+          <textarea placeholder='O que você quer perguntar?' />
+
+          <div className='form-footer'>
+            <span>
+              Para enviar uma pergunta, <button>faça seu login</button>.
+            </span>
+            <Button type='submit'>Enviar pergunta</Button>
+          </div>
+        </form>
+      </main>
+    </div>
+  )
 }
